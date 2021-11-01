8bc0fcff59b206a630d5462e719d03c317dc6e0c
davidglauber <davidglauber2010@hotmail.com>
2021-11-01 11:48:51 -0300

    [CREATE] Implementing search design, getting ready to call API

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -13,6 +13,11 @@ export default function Search({navigation}: any) {
 
             <View style={styles.searchView}>
                 <TextInput style={styles.input} placeholder="Pesquise a Cidade..." />
+                <View style={styles.searchedCities}>
+                    <Text style={{color:'white', fontWeight:'bold', fontSize:15}}>Maceió, </Text>
+                    <Text style={{color:'white', fontSize:15}}>Alagoas</Text>
+                    <Text style={{color:'white', fontWeight:'bold', fontSize:24, position:'absolute', left: width/1.4}}>24°</Text>
+                </View>
             </View>
             <StatusBar translucent={true}/>
         </View>
@@ -38,6 +43,15 @@ const styles = StyleSheet.create({
     city: {
         fontWeight:'bold',
         fontSize:18
+    },
+    searchedCities: {
+        flex:1,
+        alignItems:'center',
+        marginRight: width/1.8,
+        marginTop: 30,
+        flexDirection:"row",
+        maxHeight: height/15,
+        maxWidth: width/1.2,
     },
     input: {
         height: height/15,
